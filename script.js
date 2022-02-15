a7f0f14b4934d0423e93ade2918ce7e8f2fbd79a
Alyyen <alyssia.colomar@gmail.com>
2022-02-15 17:22:05 +0100

    dropdown de la full nav fonctionnel

--- script.js
+++ script.js
@@ -4,19 +4,26 @@
 });*/
 $(function () {
 
-// DROPDOWN
+    // DROPDOWN INSIDE NAVBAR
 
     $(".dropdown").click(function (event) {
-        let dropdowns = document.getElementsByClassName("dropdown-menu");
-        let dropdownToggle = document.getElementsByClassName("dropdown-toggle");
-
         let dropdownItems = document.getElementsByClassName("dropdown-item");
 
         for (let i = 0; i < dropdownItems.length; i++) {
             dropdownItems[i].classList.toggle("show");
             console.log(dropdownItems[i].className);
         }
+    });
 
+    // DROPDOWN FOR MOBILE NAVBAR
+
+    $(".navbar-toggler-icon").click(function (event) {
+        let dropdownItems = document.getElementsByClassName("nav-item");
+
+        for (let i = 0; i < dropdownItems.length; i++) {
+            dropdownItems[i].classList.toggle("hidden");
+            console.log(dropdownItems[i].className);
+        }
     });
 });
 
