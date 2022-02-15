78e1f491d59d8105a61e13aebe3a36765f07109f
Alyyen <alyssia.colomar@gmail.com>
2022-02-15 17:12:01 +0100

    dropdown dans la nav fonctionnel

--- script.js
+++ script.js
@@ -2,3 +2,22 @@
     $alertDanger =
     REGARDER POUR UNE MODALE
 });*/
+$(function () {
+
+// DROPDOWN
+
+    $(".dropdown").click(function (event) {
+        let dropdowns = document.getElementsByClassName("dropdown-menu");
+        let dropdownToggle = document.getElementsByClassName("dropdown-toggle");
+
+        let dropdownItems = document.getElementsByClassName("dropdown-item");
+
+        for (let i = 0; i < dropdownItems.length; i++) {
+            dropdownItems[i].classList.toggle("show");
+            console.log(dropdownItems[i].className);
+        }
+
+    });
+});
+
+
